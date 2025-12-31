5ff79c4285277e8bf53bbd9820797951291d4165
abdul <up2dul@gmail.com>
2025-12-31 09:40:53 +0700

    refactor(layout): use layout structure instead of normal component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,29 +1,23 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import BgPattern from "@/assets/bg-pattern.webp";
 import { Footer } from "@/components/layout/footer";
 import { Navbar } from "@/components/layout/navbar";
 import { cn } from "@/lib/utils";
 
-export const BaseLayout = ({
+export default function MainLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
-}>) => {
+}>) {
 	const pathname = usePathname();
 	const isNotHomePage = pathname !== "/";
 
 	return (
-		<main
-			className={cn(isNotHomePage && "pt-19 md:pt-18", "bg-center bg-repeat")}
-			style={{
-				backgroundImage: `url(${BgPattern.src})`,
-			}}
-		>
+		<main className={cn(isNotHomePage && "pt-19 md:pt-18")}>
 			<Navbar />
 			{children}
 			<Footer />
 		</main>
 	);
-};
+}

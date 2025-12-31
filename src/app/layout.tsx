dabde413c3a21fc20de09150817d54c582476f53
abdul <up2dul@gmail.com>
2025-12-31 13:39:26 +0700

    feat(layout): implement smooth scrolling with ReactLenis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { EB_Garamond, Montserrat, Rozha_One } from "next/font/google";
 import "./globals.css";
+import ReactLenis from "lenis/react";
 import BgPattern from "@/assets/bg-pattern.webp";
 import { cn } from "@/lib/utils";
 
@@ -47,6 +48,7 @@ export default function RootLayout({
 					backgroundRepeat: "repeat",
 				}}
 			>
+				<ReactLenis root />
 				{children}
 			</body>
 		</html>

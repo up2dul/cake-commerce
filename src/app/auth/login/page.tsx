8da695dff96ecc56c0ddd066084f069a7045d526
abdul <up2dul@gmail.com>
2025-12-31 11:44:19 +0700

    feat(pages/auth): update text color on login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -16,7 +16,7 @@ export default function Login() {
 
 			<section className="mt-8 mx-auto p-8 bg-white text-raisin-black w-full sm:w-4/5 md:w-3/5 lg:w-157">
 				<h1 className="font-bold text-xl md:text-2xl">RETURNING CUSTOMER</h1>
-				<p className="mt-2 text-sm md:text-base">
+				<p className="mt-2 text-sm text-raisin-black/70 md:text-base">
 					If you already have an account, please log in to continue
 				</p>
 
@@ -50,7 +50,7 @@ export default function Login() {
 
 			<section className="mt-8 mb-10 mx-auto p-8 bg-white w-full md:mb-20 sm:w-4/5 md:w-3/5 lg:w-157">
 				<h1 className="font-bold text-xl md:text-2xl">NEW CUSTOMERS</h1>
-				<p className="mt-2 text-sm md:text-base">
+				<p className="mt-2 text-sm text-raisin-black/70 md:text-base">
 					Create an account for seamless checkout experience & access to your
 					order history
 				</p>

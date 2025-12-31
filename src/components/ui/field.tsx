919ef4f0423f3f2e830e8be3ff4dcd3e034eefe8
abdul <up2dul@gmail.com>
2025-12-31 12:31:33 +0700

    feat(ui/field): change FieldDescription from p to div and update styles

--- src/components/ui/field.tsx
+++ src/components/ui/field.tsx
@@ -131,10 +131,10 @@ function FieldTitle({ className, ...props }: React.ComponentProps<"div">) {
 
 function FieldDescription({ className, ...props }: React.ComponentProps<"p">) {
 	return (
-		<p
+		<div
 			data-slot="field-description"
 			className={cn(
-				"text-muted-foreground text-left text-xs/relaxed [[data-variant=legend]+&]:-mt-1.5 leading-normal font-normal group-has-[[data-orientation=horizontal]]/field:text-balance",
+				"text-foreground text-left text-sm/5 [[data-variant=legend]+&]:-mt-1.5 leading-normal font-medium group-has-data-[orientation=horizontal]/field:text-balance",
 				"last:mt-0 nth-last-2:-mt-1",
 				"[&>a:hover]:text-primary [&>a]:underline [&>a]:underline-offset-4",
 				className,

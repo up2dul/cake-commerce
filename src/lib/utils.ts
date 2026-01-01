a839ad216527a8c06355bb682a3b230bde12d662
abdul <up2dul@gmail.com>
2026-01-01 15:26:33 +0700

    feat(utils): add format3Digit function for formatting numbers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function format3Digit(amount: number | string) {
+	let result = Number(amount);
+
+	if (result > 1000) {
+		result = result / 1000;
+	}
+
+	return result
+		.toLocaleString("en-US", {
+			maximumFractionDigits: 0,
+			minimumFractionDigits: 0,
+		})
+		.replace(/,/g, ".");
+}
